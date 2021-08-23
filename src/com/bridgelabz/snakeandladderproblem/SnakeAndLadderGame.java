ec2d292d1b60daf752b9f6fdc2edab451b87539d
akash6280 <akashsingh6280@gmail.com>
2021-08-24 01:28:39 +0530

    Two Player Game

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
@@ -1,47 +1,66 @@
 package com.bridgelabz.snakeandladderproblem;
 
 public class SnakeAndLadderGame {
-	
 	public static final int NO_PLAY=0;
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
-	
 	public static void main(String[] args) {
-		int startPosition=0;
-		int currentPosition=0;
-		int numberOfPlays=0;
-		while(currentPosition<100) {
+		
+		int currentPositionPlayer1=0;
+		int currentPositionPlayer2=0;
+		int currentPlayer=(int)Math.floor(Math.random()*10)%2+1;
+		
+		while(currentPositionPlayer1<100 && currentPositionPlayer2<100) {
+			
 			int numberOnDie=(int)Math.floor(Math.random()*10)%6+1;
 			int option=(int)Math.floor(Math.random()*10)%3;
-			numberOfPlays++;
-	
+			
 			if(option==NO_PLAY) {
 				
 			}
 			
 			if(option==LADDER) {
-				if(currentPosition+numberOnDie>100)
-				{}
-				else
-					currentPosition+=numberOnDie;
+				if(currentPlayer==1) {
+					if(currentPositionPlayer1+numberOnDie>100)
+					{}
+					else
+						currentPositionPlayer1+=numberOnDie;
+					currentPlayer=1;
+			     }
+				if(currentPlayer==2) {
+					if(currentPositionPlayer2+numberOnDie>100)
+					{}
+					else
+						currentPositionPlayer2+=numberOnDie;
+					currentPlayer=2;
+			     }
 			}
 				
 				
 			if(option==SNAKE) {
-			        currentPosition-=numberOnDie;
-			        if(currentPosition<0)
-						currentPosition=0;
-				
-		    }
-			System.out.println("Position :"+currentPosition);
-			
-			if(currentPosition==100)
-				break;
-			
-		}
-		System.out.println("NUmber of time die rolled :"+numberOfPlays);
-		System.out.println("Player Wins");
-			
-	}
+				if(currentPlayer==1) {
+			        currentPositionPlayer1-=numberOnDie;
+					if(currentPositionPlayer1<0)
+						currentPositionPlayer1=0;
+					currentPlayer=2;
+		     	}
+				if(currentPlayer==2) {
+			        currentPositionPlayer2-=numberOnDie;
+					if(currentPositionPlayer2<0)
+						currentPositionPlayer2=0;
+					currentPlayer=1;
+		    	}
+			}
+		
+			if(currentPositionPlayer2==100||currentPositionPlayer1==100)
+			break;
+		
+	} 
+		if(currentPositionPlayer1==100)
+			System.out.println("PLAYER 1 WINS");
+		if(currentPositionPlayer2==100)
+			System.out.println("PLAYER 2 WINS");
 
 }
+}
+
