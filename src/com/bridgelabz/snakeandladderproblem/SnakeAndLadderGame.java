d847ada3f070927f7a66fbc86db0b60822c7036d
akash6280 <akashsingh6280@gmail.com>
2021-08-24 00:54:29 +0530

    Rolling die to get number

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
@@ -3,6 +3,8 @@ package com.bridgelabz.snakeandladderproblem;
 public class SnakeAndLadderGame {
 	public static void main(String[] args) {
 		int startPosition=0;
+		int numberOnDie=(int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Number on die "+numberOnDie);
 	}
 
 }
