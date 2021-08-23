c089fbed4153209719a37bb3ee20971186f60450
akash6280 <akashsingh6280@gmail.com>
2021-08-24 01:04:02 +0530

    Checking option for play

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
@@ -1,10 +1,31 @@
 package com.bridgelabz.snakeandladderproblem;
 
 public class SnakeAndLadderGame {
+	
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+	
 	public static void main(String[] args) {
 		int startPosition=0;
+		int currentPosition=0;
 		int numberOnDie=(int)Math.floor(Math.random()*10)%6+1;
-		System.out.println("Number on die "+numberOnDie);
+		int option=(int)Math.floor(Math.random()*10)%3;
+		
+
+		if(option==NO_PLAY) {
+			
+		}
+		
+		if(option==LADDER) {
+			currentPosition+=numberOnDie;
+		}
+			
+			
+		if(option==SNAKE) {
+		        currentPosition-=numberOnDie;
+			
+	    }
 	}
 
 }
