44154b7873b93195d419c726fc14b97e7836d49d
akash6280 <akashsingh6280@gmail.com>
2021-08-24 00:40:23 +0530

    INITIAL COMMIT

---
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderGame.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeandladderproblem;
+
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder game");
+	}
+
+}
